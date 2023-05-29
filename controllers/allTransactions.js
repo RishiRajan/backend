0d362a99d0b1d378fc379ff65b3dabef82de6457
RishiRajan <mailrishirajan@gmail.com>
2023-05-29 18:40:21 +0530

    Route update
    updated all transactions route

---
+++ controllers/allTransactions.js
@@ -0,0 +1,20 @@
+const ExpenseSchema = require("../models/expense");
+const IncomeSchema = require("../models/income");
+
+exports.getAll = async (req, res) => {
+  try {
+    const expenses = await ExpenseSchema.find().sort({ ceratedAT: -1 });
+    const incomes = await IncomeSchema.find().sort({ ceratedAT: -1 });
+    const data = [...expenses, ...incomes];
+
+    data.sort((a, b) => {
+      if (a.date < b.date) {
+        return -1;
+      }
+    });
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ messagge: "Server Error" });
+  }
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 const{ addIncome, getIncome, deleteIncome } = require('../controllers/income');
+const { getAll } = require("../controllers/allTransactions");
 
-
-router.post('/add-income', addIncome)
-.get('/get-income',getIncome)
-.delete('/delete-income/:id',deleteIncome)
-.post('/add-expense', addExpense)
-.get('/get-expense',getExpense)
-.delete('/delete-expense/:id',deleteExpense);
+router
+  .post("/add-income", addIncome)
+  .get("/get-income", getIncome)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expense", getExpense)
+  .delete("/delete-expense/:id", deleteExpense)
+  .get("/all", getAll);
 
 module.exports =router
